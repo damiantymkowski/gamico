fc8a9704c074c9a8906ff30762806edefac9e04b
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-14 13:28:57 +0200

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import {Container} from '../styles/Header.style'
+
+const Header = () =>{
+    return(
+   <Container>
+
+   </Container>
+    );
+}
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+const IndexPage = () => (
+<>
+    <Header/>
+    <h1>Test</h1>
+</>
+)
+
+export default IndexPage
