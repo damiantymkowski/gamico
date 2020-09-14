e1e18ae9d0726638b603396a643518b0fa642d8f
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-14 14:31:02 +0200

    Add Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,20 @@
-import {Container} from '../styles/Header.style'
-
-const Header = () =>{
-    return(
-   <Container>
-
-   </Container>
+import {Container, IconContainer, Logo, Menu, MenuItem, SmallText} from '../styles/Header.style'
+import {Icon} from '@iconify/react';
+import goldIcon from '@iconify/icons-mdi/gold';
+import trophyVariantOutline from '@iconify/icons-mdi/trophy-variant-outline';
+import accountPlusOutline from '@iconify/icons-mdi/account-plus-outline';
+const Header = () => {
+    return (
+        <Container>
+            <Logo>GAMICO</Logo>
+            <Menu>
+                <MenuItem><IconContainer><Icon
+                    icon={goldIcon}/></IconContainer><SmallText>ZDOBYWAJ</SmallText></MenuItem>
+                <MenuItem><IconContainer><Icon
+                    icon={trophyVariantOutline}/></IconContainer><SmallText>LIDERZY</SmallText></MenuItem>
+                <MenuItem last><IconContainer><Icon icon={accountPlusOutline}/></IconContainer><SmallText>REJESTRACJA</SmallText></MenuItem>
+            </Menu>
+        </Container>
     );
 }
 
