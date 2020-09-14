55e54a8b3e85708a3ede28f1eab841cc5706c5e8
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-14 17:28:39 +0200

    Add responsive menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,18 @@
 import {Container, IconContainer, Logo, Menu, MenuItem, SmallText} from '../styles/Header.style'
-import {Icon} from '@iconify/react';
+import {Icon, InlineIcon} from '@iconify/react';
 import goldIcon from '@iconify/icons-mdi/gold';
 import trophyVariantOutline from '@iconify/icons-mdi/trophy-variant-outline';
 import accountPlusOutline from '@iconify/icons-mdi/account-plus-outline';
+import menuIcon from '@iconify/icons-mdi/menu';
+import MediaQuery from "react-responsive";
+
+
 const Header = () => {
     return (
         <Container>
             <Logo>GAMICO</Logo>
+
+            <MediaQuery query="(min-width: 1024px)"> {/*Desktop Menu*/}
             <Menu>
                 <MenuItem><IconContainer><Icon
                     icon={goldIcon}/></IconContainer><SmallText>ZDOBYWAJ</SmallText></MenuItem>
@@ -14,6 +20,14 @@ const Header = () => {
                     icon={trophyVariantOutline}/></IconContainer><SmallText>LIDERZY</SmallText></MenuItem>
                 <MenuItem last><IconContainer><Icon icon={accountPlusOutline}/></IconContainer><SmallText>REJESTRACJA</SmallText></MenuItem>
             </Menu>
+            </MediaQuery>
+
+            <MediaQuery query="(max-width: 1024px)">  {/*Mobile Menu*/}
+            <Menu>
+                <MenuItem last><IconContainer><Icon icon={menuIcon} /></IconContainer></MenuItem>
+            </Menu>
+            </MediaQuery>
+
         </Container>
     );
 }
