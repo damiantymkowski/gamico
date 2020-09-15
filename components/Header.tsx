508115f948006b60226badf6f0e1b19a40da9d25
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-15 11:51:39 +0200

    Code simplification with Header utils

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,6 @@
 import {Container, IconContainer, Logo, Menu, MenuItem, SmallText} from '../styles/Header.style'
-import {Icon, InlineIcon} from '@iconify/react';
-import goldIcon from '@iconify/icons-mdi/gold';
-import trophyVariantOutline from '@iconify/icons-mdi/trophy-variant-outline';
-import accountPlusOutline from '@iconify/icons-mdi/account-plus-outline';
-import menuIcon from '@iconify/icons-mdi/menu';
 import MediaQuery from "react-responsive";
-
+import {menuAccountIcon, menuGoldIcon, menuHamburgerIcon, menuTrophyIcon} from "../utils/Header.utils";
 
 const Header = () => {
     return (
@@ -14,17 +9,15 @@ const Header = () => {
 
             <MediaQuery query="(min-width: 1024px)"> {/*Desktop Menu*/}
             <Menu>
-                <MenuItem><IconContainer><Icon
-                    icon={goldIcon}/></IconContainer><SmallText>ZDOBYWAJ</SmallText></MenuItem>
-                <MenuItem><IconContainer><Icon
-                    icon={trophyVariantOutline}/></IconContainer><SmallText>LIDERZY</SmallText></MenuItem>
-                <MenuItem last><IconContainer><Icon icon={accountPlusOutline}/></IconContainer><SmallText>REJESTRACJA</SmallText></MenuItem>
+                <MenuItem><IconContainer>{menuGoldIcon()}</IconContainer><SmallText>ZDOBYWAJ</SmallText></MenuItem>
+                <MenuItem><IconContainer>{menuTrophyIcon()}</IconContainer><SmallText>LIDERZY</SmallText></MenuItem>
+                <MenuItem last><IconContainer>{menuAccountIcon()}</IconContainer><SmallText>REJESTRACJA</SmallText></MenuItem>
             </Menu>
             </MediaQuery>
 
             <MediaQuery query="(max-width: 1024px)">  {/*Mobile Menu*/}
             <Menu>
-                <MenuItem last><IconContainer><Icon icon={menuIcon} /></IconContainer></MenuItem>
+                <MenuItem last><IconContainer>{menuHamburgerIcon()}</IconContainer></MenuItem>
             </Menu>
             </MediaQuery>
 
