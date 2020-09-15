c5a7a44207b90533c14462ef091d693ebb1fbe03
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-15 11:50:54 +0200

    Add Header utils with icons

---
+++ utils/Header.utils.tsx
@@ -0,0 +1,29 @@
+import {Icon, InlineIcon} from '@iconify/react';
+import goldIcon from '@iconify/icons-mdi/gold';
+import trophyVariantOutline from '@iconify/icons-mdi/trophy-variant-outline';
+import accountPlusOutline from '@iconify/icons-mdi/account-plus-outline';
+import menuIcon from '@iconify/icons-mdi/menu';
+
+export const menuGoldIcon = () => {
+    return(
+        <Icon icon={goldIcon}/>
+    );
+}
+
+export const menuTrophyIcon = () => {
+    return(
+        <Icon icon={trophyVariantOutline}/>
+    );
+}
+
+export const menuAccountIcon = () => {
+    return(
+        <Icon icon={accountPlusOutline}/>
+    );
+}
+
+export const menuHamburgerIcon = () => {
+    return(
+        <Icon icon={menuIcon}/>
+    );
+}
