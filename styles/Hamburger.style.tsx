3a173de9c57c4c7409400f67d96545b51e2da060
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-16 14:25:28 +0200

    Add CSS animations to hamburger icon

--- styles/Hamburger.style.tsx
+++ styles/Hamburger.style.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 import {Colors} from "./Colors.style";
 
 export const HamburgerButton = styled.button`
@@ -9,7 +9,11 @@ width: 40px;
 height: 40px;
 text-align: left;
 cursor: pointer;
+margin-left: auto;
+padding: 0;
+margin-right: 15px;
 `
+
 
 export const HamburgerIcon = styled.span`
 width: 20px;
@@ -20,14 +24,14 @@ position: relative;
 export const HamburgerLine = styled.span`
 content: '';
 position: absolute;
-border-bottom: 1px solid ${Colors.white};
+border-bottom: 1.5px solid ${Colors.lime};
 width: 30px;
 height: 5px;
 left:0;
 &::before, &::after{
 content: '';
 position: absolute;
-border-bottom: 1px solid ${Colors.white};
+border-bottom: 1.5px solid ${Colors.lime};
 width: 30px;
 height: 5px;
 }
@@ -37,6 +41,38 @@ top:-10px;
 &::after{
 top: 10px;
 }
-
 `
+
+const openMenuAnimationAfter = keyframes`
+    0% { transform: translateX(-3px) translateY(0px) rotate(0deg)}
+    30% { transform: translateX(-3px) translateY(-10px) rotate(-20deg) }
+    40% { transform: translateX(-3px) translateY(-10px) rotate(-30deg) }
+    100% { transform: translateX(-3px) translateY(-10px) rotate(-45deg) }
+`
+
+const openMenuAnimationBefore = keyframes`
+    0% { transform: translateY(0px) rotate(0deg) }
+    30% { transform: translateY(10px) rotate(20deg)}
+    40% { transform: translateY(10px) rotate(30deg)}
+    100% { transform: translateY(10px) rotate(45deg)}
+`
+
+export const ActiveHamburgerButton = styled(HamburgerButton)`
+${HamburgerLine}{
+position: relative;
+    &::after{
+    transform: translateY(-10px) rotate(-40deg);
+    animation-name: ${openMenuAnimationAfter};
+    animation-duration: 0.5s;
+    animation-fill-mode: forwards;
+    }
+    &::before{
+    transform: translateY(10px) rotate(40deg);
+    animation-name: ${openMenuAnimationBefore};
+    animation-duration: 0.5s;
+    animation-fill-mode: forwards;
+    }
+}
+`
+
 
