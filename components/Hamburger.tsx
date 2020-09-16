c6023f2a8f56f8436102f4e761cac051348a9d51
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-16 14:24:41 +0200

    Add ref to DOM element from children component & state

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -1,12 +1,24 @@
-import {HamburgerButton, HamburgerIcon, HamburgerLine} from "../styles/Hamburger.style";
+import {ActiveHamburgerButton, HamburgerButton, HamburgerIcon, HamburgerLine} from "../styles/Hamburger.style";
+import {useEffect, useState} from "react";
 
-const Hamburger = () =>{
+const Hamburger = ({mobileMenuElement}) =>{
+    const [showMenu, setShowMenu] = useState(false);
+    const Hamburger = showMenu ? ActiveHamburgerButton : HamburgerButton;
+    const toggleMobileMenu = () => {
+        if (showMenu == false) {
+            mobileMenuElement.current.style.display = "flex";
+            setShowMenu(true);
+        } else {
+            mobileMenuElement.current.style.display = "none";
+            setShowMenu(false);
+        }
+    };
     return(
-       <HamburgerButton>
+       <Hamburger onClick={toggleMobileMenu}>
            <HamburgerIcon>
                <HamburgerLine></HamburgerLine>
            </HamburgerIcon>
-       </HamburgerButton>
+       </Hamburger>
     );
 }
 
