40858e331b9b794670d5a13b2a003e1027f8cce2
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-19 14:55:54 +0200

    Add register page component

---
+++ pages/register.tsx
@@ -0,0 +1,13 @@
+import {GlobalStyle} from "../styles/Global.style";
+import Header from "../components/Header";
+
+const Register = () =>{
+    return(
+      <>
+          <GlobalStyle/>
+          <Header/>
+      </>
+    );
+}
+
+export default Register
