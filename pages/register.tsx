7ec5497933ac8effd2e28c4fe103a1525a325c28
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-23 22:05:21 +0200

    Add register box

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,11 +1,31 @@
 import {GlobalStyle} from "../styles/Global.style";
 import Header from "../components/Header";
+import {
+    Container,
+    RegisterBox,
+    RegisterBoxForm,
+    RegisterBoxImage,
+    RegisterBoxInfo,
+    RegisterBoxTitle
+} from "../styles/Register.style";
+
 
 const Register = () =>{
     return(
       <>
           <GlobalStyle/>
           <Header/>
+          <Container>
+    <RegisterBox>
+        <RegisterBoxImage alt="Register box image. Minecraft character with potion. He have long robe, big smile and tshirt." src="/images/doktor.png" />
+        <RegisterBoxForm>
+        <RegisterBoxTitle>REJESTRACJA</RegisterBoxTitle>
+        <RegisterBoxInfo>OTRZYMAJ DOSTĘP DO WSZYSTKICH FUNKCJI</RegisterBoxInfo>
+
+        </RegisterBoxForm>
+
+        </RegisterBox>
+          </Container>
       </>
     );
 }
