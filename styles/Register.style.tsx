b5b6db730cbe4feabf9b2283535e39cb777d11d1
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-28 15:55:55 +0200

    Add register box styles

--- styles/Register.style.tsx
+++ styles/Register.style.tsx
@@ -24,10 +24,9 @@ color: ${Colors.lime};
 font-family: 'Karla', sans-serif;
 `;
 
-export const RegisterBoxForm = styled.div`
-flex: 50%;
-margin-left: -20%;
-margin-top: -100px;
+export const RegisterBoxForm = styled.form`
+flex: 95%;
+margin-left: -35%;
 `;
 
 export const RegisterBoxInfo = styled.h3`
@@ -43,8 +42,47 @@ white-space: pre;
 
 export const RegisterBoxImage = styled.img`
 right: 70px;
-flex: 10%;
+flex: 5%;
 position: relative;
 top: 150px;
-
 `;
+
+export const RegisterBoxInput = styled.input`
+width: 50%;
+margin-left: 75px;
+height: 30px;
+margin-top: 5px;
+border: none;
+`;
+
+export const RegisterBoxButton = styled.button`
+width: 50.5%;
+margin-left: 75px;
+padding: 5px;
+margin-top: 5px;
+border: none;
+background-color: ${Colors.lime};
+font-family: Rubik, sans-serif;
+font-size: 17px;
+cursor: pointer;
+`
+
+export const RegisterBoxInputContainer = styled.div`
+  top: 12px;
+  left: 15px;
+`
+
+export const RegisterBoxInputImg = styled.div`
+position: absolute;
+margin-left: 138.2px;
+margin-top: 5px;
+height: 32.1px;
+width: 36px;
+background-color: ${Colors.lime};
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 25px;
+color: ${Colors.darkBlue};
+`
+
