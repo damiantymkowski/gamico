f4ee20986b62151726f94d34821e705233246110
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-28 15:55:21 +0200

    Update register box, add inputs

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,12 +3,16 @@ import Header from "../components/Header";
 import {
     Container,
     RegisterBox,
+    RegisterBoxButton,
     RegisterBoxForm,
     RegisterBoxImage,
     RegisterBoxInfo,
+    RegisterBoxInput,
+    RegisterBoxInputContainer,
+    RegisterBoxInputImg,
     RegisterBoxTitle
 } from "../styles/Register.style";
-
+import {accountIcon, lockIcon, mailIcon,} from "../utils/Icons";
 
 const Register = () =>{
     return(
@@ -17,11 +21,25 @@ const Register = () =>{
           <Header/>
           <Container>
     <RegisterBox>
-        <RegisterBoxImage alt="Register box image. Minecraft character with potion. He have long robe, big smile and tshirt." src="/images/doktor.png" />
+        <RegisterBoxImage
+            alt="Register box image. Minecraft character with potion. He have long robe, big smile and tshirt."
+            src="/images/doktor.png"/>
         <RegisterBoxForm>
-        <RegisterBoxTitle>REJESTRACJA</RegisterBoxTitle>
-        <RegisterBoxInfo>OTRZYMAJ DOSTĘP DO WSZYSTKICH FUNKCJI</RegisterBoxInfo>
-
+            <RegisterBoxTitle>REJESTRACJA</RegisterBoxTitle>
+            <RegisterBoxInfo>OTRZYMAJ DOSTĘP DO WSZYSTKICH FUNKCJI</RegisterBoxInfo>
+            <RegisterBoxInputContainer>
+                <RegisterBoxInputImg>{mailIcon()}</RegisterBoxInputImg>
+                <RegisterBoxInput placeholder="Adres e-mail"></RegisterBoxInput>
+            </RegisterBoxInputContainer>
+            <RegisterBoxInputContainer>
+                <RegisterBoxInputImg>{accountIcon()}</RegisterBoxInputImg>
+                <RegisterBoxInput placeholder="Nazwa Użytkownika"></RegisterBoxInput>
+            </RegisterBoxInputContainer>
+            <RegisterBoxInputContainer>
+                <RegisterBoxInputImg>{lockIcon()}</RegisterBoxInputImg>
+                <RegisterBoxInput placeholder="Hasło" type="password"></RegisterBoxInput>
+            </RegisterBoxInputContainer>
+            <RegisterBoxButton>ZAREJESTRUJ SIĘ</RegisterBoxButton>
         </RegisterBoxForm>
 
         </RegisterBox>
