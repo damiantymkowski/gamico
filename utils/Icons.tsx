4dc3f7b67f403c66bb801af1cc1914d5acc7d2a5
Damian Tymkowski <thedamianpl@gmail.com>
2020-09-28 15:56:08 +0200

    Add new icons

--- utils/Icons.tsx
+++ utils/Icons.tsx
@@ -4,6 +4,20 @@ import trophyVariantOutline from '@iconify/icons-mdi/trophy-variant-outline';
 import accountPlusOutline from '@iconify/icons-mdi/account-plus-outline';
 import menuIcon from '@iconify/icons-mdi/menu';
 import emailIcon from '@iconify/icons-mdi/email';
+import lockQuestion from '@iconify/icons-mdi/lock-question';
+import accountBox from '@iconify/icons-mdi/account-box';
+
+export const accountIcon = () => {
+    return(
+        <Icon icon={accountBox} />
+    );
+}
+
+export const lockIcon = () => {
+    return(
+        <Icon icon={lockQuestion} />
+    );
+}
 
 export const mailIcon = () => {
     return(
