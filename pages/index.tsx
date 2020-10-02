20145fbf960e5075d190088bf0d83890a357dd35
Damian Tymkowski <thedamianpl@gmail.com>
2020-10-02 18:49:21 +0200

    Add new text blocks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import Header from "../components/Header";
 import {GlobalStyle} from "../styles/Global.style";
 import {
+    AboutHeader,
+    AboutSiteText,
+    AboutTextContainer,
     Banner,
     BannerMainImage,
     BannerSmallText,
     BannerTitle,
     Container,
     IndexTitle,
-    InfoBlock, InfoContainer
+    InfoBlock, InfoContainer, InfoIcon, InfoSmallText, InfoTitle, StatsBlock, StatsContainer
 } from "../styles/Index.style";
 import {motion} from "framer-motion";
+import Icon from "@iconify/react";
+import cash100 from '@iconify/icons-mdi/cash-100';
+import React from "react";
 
 const IndexPage = () => {
 
@@ -30,8 +36,41 @@ const IndexPage = () => {
             </Banner>
                 <IndexTitle>JAK TO DZIAŁA?</IndexTitle>
     <InfoContainer>
-<InfoBlock></InfoBlock><InfoBlock></InfoBlock><InfoBlock></InfoBlock>
+        <InfoBlock>
+            <InfoIcon><Icon icon={cash100} /></InfoIcon>
+            <InfoTitle>ZARABIAJ</InfoTitle>
+            <InfoSmallText>Wygrywaj różnorodne nagrody za artykuły</InfoSmallText>
+        </InfoBlock>
+        <InfoBlock>
+            <InfoIcon><Icon icon={cash100} /></InfoIcon>
+            <InfoTitle>ZARABIAJ</InfoTitle>
+            <InfoSmallText>Wygrywaj różnorodne nagrody za artykuły</InfoSmallText>
+        </InfoBlock>
+        <InfoBlock>
+            <InfoIcon><Icon icon={cash100} /></InfoIcon>
+            <InfoTitle>ZARABIAJ</InfoTitle>
+            <InfoSmallText>Wygrywaj różnorodne nagrody za artykuły</InfoSmallText>
+        </InfoBlock>
     </InfoContainer>
+
+    <StatsContainer>
+        <StatsBlock>ZEBRANYCH MONET</StatsBlock>
+        <StatsBlock>UŻYTKOWNIKÓW</StatsBlock>
+        <StatsBlock>NAPISANYCH ARTYKUŁÓW</StatsBlock>
+    </StatsContainer>
+
+    <AboutTextContainer>
+        <AboutSiteText>
+            <AboutHeader>Tekst SEO</AboutHeader>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit aliquam faucibus leo habitasse volutpat, sit. Pretium mi nunc dui porta tellus tellus mauris, pharetra consectetur. Sit neque nunc ullamcorper amet, eget ornare pellentesque convallis. Faucibus enim enim tellus eleifend condimentum erat praesent. Nulla tellus ut tortor justo, neque lorem cum odio. Tincidunt et turpis aliquam dignissim tincidunt nec quam faucibus. Senectus eget maecenas duis purus duis non morbi sed. Commodo hac sapien id tristique.
+        </AboutSiteText>
+        <AboutSiteText>
+            <AboutHeader>Tekst SEO</AboutHeader>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit aliquam faucibus leo habitasse volutpat, sit. Pretium mi nunc dui porta tellus tellus mauris, pharetra consectetur. Sit neque nunc ullamcorper amet, eget ornare pellentesque convallis. Faucibus enim enim tellus eleifend condimentum erat praesent. Nulla tellus ut tortor justo, neque lorem cum odio. Tincidunt et turpis aliquam dignissim tincidunt nec quam faucibus. Senectus eget maecenas duis purus duis non morbi sed. Commodo hac sapien id tristique.
+        </AboutSiteText>
+    </AboutTextContainer>
+
+
 </Container>
             </motion.div>
         </>
