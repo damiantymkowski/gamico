dc6b8b0310af1347c771b312c912bcb249ccf7bd
Damian Tymkowski <thedamianpl@gmail.com>
2020-10-02 14:56:54 +0200

    Add InfoBlock styles

--- styles/Index.style.tsx
+++ styles/Index.style.tsx
@@ -5,7 +5,6 @@ export const Banner = styled.div`
 min-height: 300px;
 background: ${Colors.lightGray};
 margin-top: 20px;
-position: absolute;
 width: 100%;
 display: flex;
 justify-content: center;
@@ -30,4 +29,45 @@ font-weight: 200;
 
 export const BannerMainImage = styled.div`
 
+`
+
+export const IndexTitle = styled.h2`
+font-family: 'Rubik', sans-serif;
+font-size: 36px;
+color: ${Colors.white};
+position: relative;
+`
+
+export const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`
+
+export const InfoContainer = styled.div`
+display: flex;
+flex-direction: row;
+`
+
+export const InfoBlock = styled.div`
+width: 225px;
+height: 175px;
+background-color: red;
+display: block;
+position: relative;
+padding: 40px;
+&:nth-child(1){
+background-color: red;
+position: relative;
+left: 50px;
+}
+&:nth-child(2){
+background-color: white;
+position: relative;
+z-index: 999;
+}
+&:nth-child(3){
+right: 50px;
+}
 `
