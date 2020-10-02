83cd16d201e067a778cce0c90bf2a95169af18c0
Damian Tymkowski <thedamianpl@gmail.com>
2020-10-02 14:56:15 +0200

    Add InfoBlock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import Header from "../components/Header";
 import {GlobalStyle} from "../styles/Global.style";
-import {Banner, BannerMainImage, BannerSmallText, BannerTitle} from "../styles/Index.style";
+import {
+    Banner,
+    BannerMainImage,
+    BannerSmallText,
+    BannerTitle,
+    Container,
+    IndexTitle,
+    InfoBlock, InfoContainer
+} from "../styles/Index.style";
 import {motion} from "framer-motion";
 
 const IndexPage = () => {
@@ -14,11 +22,17 @@ const IndexPage = () => {
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
             >
+<Container>
             <Banner>
                 <BannerTitle>Pisz artykuły o grach</BannerTitle>
                 <BannerSmallText>zdobywaj rozmaite nagrody</BannerSmallText>
                 <BannerMainImage></BannerMainImage>
             </Banner>
+                <IndexTitle>JAK TO DZIAŁA?</IndexTitle>
+    <InfoContainer>
+<InfoBlock></InfoBlock><InfoBlock></InfoBlock><InfoBlock></InfoBlock>
+    </InfoContainer>
+</Container>
             </motion.div>
         </>
     );
