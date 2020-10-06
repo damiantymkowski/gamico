b57b5def1eea70e02179af31465fc4103da6c8eb
Damian Tymkowski <thedamianpl@gmail.com>
2020-10-06 23:20:56 +0200

    Add container

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import {Container} from "../styles/Footer";
+
+const Footer = () => {
+    return(
+      <>
+         <Container>
+             Lorem ipsum
+      </Container>
+          </>
+    );
+}
+
+export default Footer
