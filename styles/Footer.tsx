d4683cfdc1851006c85b066f1a0192143cc06c99
Damian Tymkowski <thedamianpl@gmail.com>
2020-10-06 23:21:23 +0200

    Update footer container style

---
+++ styles/Footer.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import {Colors} from "./Colors.style";
+
+export const Container = styled.div`
+margin-top: 40px;
+width: 100%;
+height: 50px;
+background-color: ${Colors.lightGray};
+color: ${Colors.white};
+display: flex;
+justify-content: center;
+align-items: center;
+`
