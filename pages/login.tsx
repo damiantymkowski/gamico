cecaa3066d6303f4266494f4b00148c612a1c0df
Damian Tymkowski <thedamianpl@gmail.com>
2020-10-11 15:16:57 +0200

    Update login page

---
+++ pages/login.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { GlobalStyle } from "../styles/Global.style";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import * as S from "../styles/Login.style";
+
+const Login = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <S.Container>
+       <S.Box>
+           
+       </S.Box>
+      </S.Container>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
