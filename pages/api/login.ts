dd48e110425959af9d7415aa741e91dd9925b92c
Damian Tymkowski <thedamianpl@gmail.com>
2020-10-11 15:17:31 +0200

    Add login API

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,44 +1,47 @@
-import {NextApiRequest, NextApiResponse} from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import connectToDb from "./db";
 const User = require("./models/user");
-import bcrypt from 'bcryptjs';
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    await connectToDb();
-    User.find({email: req.body.email})
-        .exec()
-        .then(user=>{
-            if(user.length<1){
-                return res.status(401).json({
-                    message: 'Auth failed'
-                });
-            }
-            bcrypt.compare(req.body.password, user[0].password, (err,result)=>{
-                if(err){
-                    return res.status(401).json({
-                        message: 'Auth failed'
-                    });
-                }
-                if(result){
-                    const token = jwt.sign({
-                            email: user[0].email,
-                            nickname: user[0].login
-                        },
-                        "13GRV!%asfXXCfQ", {expiresIn: "1h"})
-                    return res.status(200).json({
-                        message: 'Auth successful',
-                        token: token
-                    });
-                }
-                res.status(401).json({
-                    message: "Auth failed"
-                });
-            });
-        })
-        .catch(err=>{
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
+  await connectToDb();
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
         });
-}
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              nickname: user[0].login,
+            },
+            "13GRV!%asfXXCfQ",
+            { expiresIn: "1h" }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
